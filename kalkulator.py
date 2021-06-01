7297a25f6a361fe0b1e88cb03927f2a9520742f5
bartek5936 <bartek5936@gmail.com>
2021-06-01 12:53:56 +0200

    kalkulation

---
+++ kalkulator.py
@@ -0,0 +1,33 @@
+import datetime
+
+print("Kalkulator")
+b = "1"
+while not b == '0':
+    aa = input("Podaj co chcesz robic \n 1. Dodawania \n 2. Odejmowanie \n 3. Mnozenie\n 4. Dzielenie\n")
+    a = int(input("Podaj liczbe"))
+    b = int(input("Podaj liczbe"))
+    wynik = 0
+
+    if aa == '1':
+        wynik = a + b
+        print(wynik)
+    if aa == '2':
+        wynik = a - b
+        print(wynik)
+    if aa == '3':
+        wynik = a*b
+        print(wynik)
+    if aa == "4":
+        try:
+            wynik = a / b
+            print(wynik)
+        except (ZeroDivisionError):
+
+            print("nie dziel przez zero")
+
+    plik = open("../../Users/Bartłomiej/PycharmProjects/pythonProject/wynikdzi2alania.txt", "w")
+    plik.write(str(wynik))
+    if b == 0:
+        break
+
+
